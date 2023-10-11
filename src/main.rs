fc00c0db274c609a0f14e02aa60e7e51371be554
ahuoguo <ahuoguo@gmail.com>
2023-10-11 07:07:33 -0400

    have something that works for string-dsl

--- src/main.rs
+++ src/main.rs
@@ -1,31 +1,24 @@
 use std::collections::HashMap;
-// use generator::*;
-
+use std::io::Write;
 mod string_dsl;
 
-use itertools::Itertools;
-use string_dsl::*;
-use ::next_gen::prelude::*;
+use itertools::{Itertools, MultiProduct};
 
+use ::next_gen::prelude::*;
+use string_dsl::*;
 
 fn main() {
     // x[0..find(x," ")]
     let program1 = S::SubString(
         Box::new(S::Input),
         Box::new(N::Zero),
-        Box::new(N::Find(
-            S::Input,
-            S::Space
-        ))
+        Box::new(N::Find(S::Input, S::Space)),
     );
 
     // x[0..find(x," ")]+" "+x[0..find(x," ")]
-    let _program2 = S::Append(
+    let program2 = S::Append(
         Box::new(program1.clone()),
-        Box::new(S::Append(
-            Box::new(S::Space),
-            Box::new(program1.clone())
-        ))
+        Box::new(S::Append(Box::new(S::Space), Box::new(program1.clone()))),
     );
 
     let input1 = "Nadia Polikarpova".to_string();
@@ -36,262 +29,165 @@ fn main() {
     let output2 = eval(program1, input2.clone());
     println!("Output2 is: {}", output2);
 
-    // let input3 = "Nadia Polikarpova".to_string();
-    // let output3 = eval(program2.clone(), input3);
-    // println!("Output3 is: {}", output3);
+    let input3 = "Nadia Polikarpova".to_string();
+    let output3 = eval(program2.clone(), input3.clone());
+    println!("Output3 is: {}", output3);
 
-    // let input4 = "Loris D\'Antoni".to_string();
-    // let output4 = eval(program2, input4);
-    // println!("Output4 is: {}", output4);
-    
+    let input4 = "Loris D\'Antoni".to_string();
+    let output4 = eval(program2, input4.clone());
+    println!("Output4 is: {}", output4);
+
+    // this program synthesizes really fast
     bottom_up_synthesis(vec![(input1, output1), (input2, output2)]);
-}
 
+    bottom_up_synthesis(vec![(input3, output3), (input4, output4)]);
+}
 
 // bottom up synthesis algorithm
 // the member ship oracle with the input is represented by e, the input output pair
 // the output is the program, with start node S
 // I do not abstract away the grammar for now
-fn bottom_up_synthesis(_e: Vec<(String, String)>) -> S {
+fn bottom_up_synthesis(e: Vec<(String, String)>) -> S {
+    let mut b: HashMap<(u32, NonTerminal), Vec<Expr>> = HashMap::new();
 
-    // temporary fix here..., it is hard to make a global vector in rust since allocation cannot happen in const/static
-    // let production:Vec<(NonTerminal, u32, Transition, Vec<NonTerminal>) > = vec![
-    // (NonTerminal::S, 0, Transition::Input, vec![]),
-    // (NonTerminal::S, 0, Transition::Space, vec![]),
-    // (NonTerminal::S, 1, Transition::Append, vec![NonTerminal::S, NonTerminal::S]),
-    // (NonTerminal::S, 2, Transition::SubString, vec![NonTerminal::S, NonTerminal::N, NonTerminal::N]),
-    // (NonTerminal::N, 0, Transition::Zero, vec![]),
-    // (NonTerminal::N, 1, Transition::Find, vec![NonTerminal::S, NonTerminal::S])
-    // ];
-
-
-    let mut b:HashMap<(u32, NonTerminal), Vec<Expr>> = HashMap::new();
-
-    for n in 0..10 { // assume the max height is 10
+    for n in 0..10 {
+        // assume the max height is 10
         mk_gen!(let new_terms_res = new_terms(n, b.clone()));
         for (a, t) in new_terms_res {
-            eprintln!("a is {:?}, t is {:?}", a.clone(), t.clone());
-            eprintln!("b is {:?}", b.clone());
-            b.entry((n, a)).or_insert(Vec::new()).push(t);
-            eprintln!("hihihi");
-        }
-    }
-
-    return S::Input;
-}
-
-
-// fn new_terms<'a>(n: u32, b: HashMap<(u32, NonTerminal), Vec<Expr>>) -> Generator<'static, (), (NonTerminal, Expr)> {
-//     Gn::new_scoped(move |mut s| {
-//         // for all grammar productions
-
-//         // base case, arity and height are 0
-//         // if n == 0 && k == 0 {
-//         //   return (NonTerminal::S, S::Input);
-
-//         // cannot implement Copy trait for recursive enum (with Box)
-//         for (_nt, k, transition, subnt ) in string_dsl::PRODUCTION {
-//             if *k == 0 && n == 0 {
-//                 match transition {
-//                     Transition::Input => {
-//                         s.yield_with((NonTerminal::S, Expr::S(S::Input)));
-//                     },
-//                     Transition::Space => {
-//                         s.yield_with((NonTerminal::S, Expr::S(S::Space)));
-//                     },
-//                     Transition::Zero => {
-//                         s.yield_with((NonTerminal::N, Expr::N(N::Zero)));
-//                     },
-//                     _ => { 
-//                         panic!{"production list encoded with errorneous information"}
-//                     }
-//                 }
-//             } else {
-//                 // build subterms from the bank
-//                 eprintln!("sub-nonterminals are {:?} n is {}, k is {}, transition: {:?}", subnt, n, k, transition);
-//                 let heights = (0..n).permutations(*k as usize).collect_vec();
-//                 eprintln!("heights are {:?}", heights);
-//                 if heights.is_empty() {
-//                     eprintln!("continue");
-//                     continue;
-//                 }
-//                 for ns in heights.clone() {
-//                     if !ns.contains(&(n-1)) {
-//                         continue;
-//                     }
-//                     let mut subterms:Vec<Vec<Expr>> = Vec::new();
-//                     for i in 0..*k {
-//                         println!("i is {}", i);
-//                         eprintln!("ns[i] is {}, subnt[i] is {:?}", ns[i as usize], subnt[i as usize]);
-//                         let subterm = b.get(&(ns[i as usize], subnt[i as usize])).unwrap();
-//                         subterms.push(subterm.clone());
-//                     }
-//                     // for subterm in subterms.iter().multi_cartesian_product() {
-//                     //     match transition {
-//                     //         Transition::Append => {
-//                     //             match (subterm[0].clone(), subterm[1].clone()) {
-//                     //                 (Expr::S(s1), Expr::S(s2)) => {
-//                     //                     s.yield_with((NonTerminal::S, Expr::S(S::Append(Box::new(s1), Box::new(s2)))));
-//                     //                 },
-//                     //                 _ => {
-//                     //                     panic!{"production list encoded with errorneous information"}
-//                     //                 }
-//                     //             }
-//                     //         },
-//                     //         Transition::SubString => {
-//                     //             match (subterm[0].clone(), subterm[1].clone(), subterm[2].clone()) {
-//                     //                 (Expr::S(s1), Expr::N(n1), Expr::N(n2)) => {
-//                     //                     s.yield_with((NonTerminal::S, Expr::S(S::SubString(Box::new(s1), Box::new(n1), Box::new(n2))))); 
-//                     //                 },
-//                     //                 _ => {
-//                     //                     panic!{"production list encoded with errorneous information"}
-//                     //                 }
-//                     //             }
-//                     //         },
-//                     //         Transition::Find => {
-//                     //             match (subterm[0].clone(), subterm[1].clone()) {
-//                     //                 (Expr::S(s1), Expr::S(s2)) => {
-//                     //                     s.yield_with((NonTerminal::N, Expr::N(N::Find(s1, s2))));
-//                     //                 },
-//                     //                 _ => {
-//                     //                     panic!{"production list encoded with errorneous information"}
-//                     //                 }
-//                     //             }
-//                     //         },
-//                     //         _ => { 
-//                     //             panic!{"production list encoded with errorneous information"}
-//                     //         }
-//                     //     }
-//                     // }
-
-//                 }
-//             }
-
-//         }
-//         done!();
-//     })
-// }
-
-#[generator(yield((NonTerminal, Expr)))]
-fn new_terms (n: u32, b: HashMap<(u32, NonTerminal), Vec<Expr>>)
-{
-        // for all grammar productions
-
-        // base case, arity and height are 0
-        // if n == 0 && k == 0 {
-        //   return (NonTerminal::S, S::Input);
-
-        // cannot implement Copy trait for recursive enum (with Box)
-        for (_nt, k, transition, subnt ) in string_dsl::PRODUCTION {
-            if *k == 0 && n == 0 {
-                match transition {
-                    Transition::Input => {
-                        yield_!((NonTerminal::S, Expr::S(S::Input)));
-                    },
-                    Transition::Space => {
-                        yield_!((NonTerminal::S, Expr::S(S::Space)));
-                    },
-                    Transition::Zero => {
-                        yield_!((NonTerminal::N, Expr::N(N::Zero)));
-                    },
-                    _ => { 
-                        panic!{"production list encoded with errorneous information"}
+            // eprintln!("a is {:?}, t is {:?}", a.clone(), t.clone());
+            if a == NonTerminal::S {
+                match t.clone() {
+                    Expr::S(s) => {
+                        println!("{}", s.clone());
+                        for (input, output) in e.clone() {
+                            std::io::stdout().flush().unwrap();
+                            if eval(s.clone(), input.clone()) == output {
+                                println!("Found the program: {:?}", s);
+                                return s;
+                            }
+                        }
                     }
-                }
-            } else {
-                // build subterms from the bank
-                eprintln!("sub-nonterminals are {:?} n is {}, k is {}, transition: {:?}", subnt, n, k, transition);
-                let heights = (0..n).permutations(*k as usize).collect_vec();
-                eprintln!("heights are {:?}", heights);
-                if heights.is_empty() {
-                    eprintln!("continue");
-                    continue;
-                }
-                for ns in heights.clone() {
-                    if !ns.contains(&(n-1)) {
-                        continue;
+                    _ => {
+                        panic! {"NonTerminal and Expr is not aligned"}
                     }
-                    let mut subterms:Vec<Vec<Expr>> = Vec::new();
-                    for i in 0..*k {
-                        println!("i is {}", i);
-                        eprintln!("ns[i] is {}, subnt[i] is {:?}", ns[i as usize], subnt[i as usize]);
-                        let subterm = b.get(&(ns[i as usize], subnt[i as usize])).unwrap();
-                        subterms.push(subterm.clone());
-                    }
-                    // for subterm in subterms.iter().multi_cartesian_product() {
-                    //     match transition {
-                    //         Transition::Append => {
-                    //             match (subterm[0].clone(), subterm[1].clone()) {
-                    //                 (Expr::S(s1), Expr::S(s2)) => {
-                    //                     s.yield_with((NonTerminal::S, Expr::S(S::Append(Box::new(s1), Box::new(s2)))));
-                    //                 },
-                    //                 _ => {
-                    //                     panic!{"production list encoded with errorneous information"}
-                    //                 }
-                    //             }
-                    //         },
-                    //         Transition::SubString => {
-                    //             match (subterm[0].clone(), subterm[1].clone(), subterm[2].clone()) {
-                    //                 (Expr::S(s1), Expr::N(n1), Expr::N(n2)) => {
-                    //                     s.yield_with((NonTerminal::S, Expr::S(S::SubString(Box::new(s1), Box::new(n1), Box::new(n2))))); 
-                    //                 },
-                    //                 _ => {
-                    //                     panic!{"production list encoded with errorneous information"}
-                    //                 }
-                    //             }
-                    //         },
-                    //         Transition::Find => {
-                    //             match (subterm[0].clone(), subterm[1].clone()) {
-                    //                 (Expr::S(s1), Expr::S(s2)) => {
-                    //                     s.yield_with((NonTerminal::N, Expr::N(N::Find(s1, s2))));
-                    //                 },
-                    //                 _ => {
-                    //                     panic!{"production list encoded with errorneous information"}
-                    //                 }
-                    //             }
-                    //         },
-                    //         _ => { 
-                    //             panic!{"production list encoded with errorneous information"}
-                    //         }
-                    //     }
-                    // }
-
                 }
             }
-
+            b.entry((n, a)).or_insert(Vec::new()).push(t);
+            // eprintln!("b is {:?}", b.clone());
         }
+    }
+    panic! {"No program found"}
 }
 
+#[generator(yield((NonTerminal, Expr)))]
+fn new_terms(n: u32, b: HashMap<(u32, NonTerminal), Vec<Expr>>) {
+    // for all grammar productions
 
-// fn allcombinations(n:u32, k:u32) -> Vec<Vec<u32>> {
-//     // create a vector of vectors, each with length k, and each element is a number from 0 to n-1,
-//     // they also need to have one index equal to n-1
-//     // there are n * n * ... * n = n^k such vectors
-//     let mut result:Vec<Vec<u32>> = Vec::new();
-//     for i in 0..k {
-//         let mut v:Vec<u32> = Vec::new();
-//         for j in 0..k {
-//             if j == i {
-//                 v.push(n-1);
-//             } else {
-//             }
-//         }
-//         result.push(v);
-//     }
-//     return result;
-// }
+    // base case, arity and height are 0
+    // if n == 0 && k == 0 {
+    //   return (NonTerminal::S, S::Input);
 
-// // note that new terms is implemented as an interator
-// fn new_terms(n:u32, B:HashMap<(u32, NonTerminal), Vec<Expr>>) -> (NonTerminal, Expr) {
-//     // for all grammar productions
-//     for 
+    // cannot implement Copy trait for recursive enum (with Box)
+    for (_nt, k, transition, subnt) in string_dsl::PRODUCTION {
+        if *k == 0 && n == 0 {
+            match transition {
+                Transition::Input => {
+                    yield_!((NonTerminal::S, Expr::S(S::Input)));
+                }
+                Transition::Space => {
+                    yield_!((NonTerminal::S, Expr::S(S::Space)));
+                }
+                Transition::Zero => {
+                    yield_!((NonTerminal::N, Expr::N(N::Zero)));
+                }
+                _ => {
+                    panic! {"production list encoded with errorneous information"}
+                }
+            }
+        } else {
+            // build subterms from the bank
+            // eprintln!("sub-nonterminals are {:?} n is {}, k is {}, transition: {:?}", subnt, n, k, transition);
+            let heights = (0..n).product_repeat(*k as usize).collect_vec();
+            for ns in heights.clone() {
+                if !ns.contains(&(n - 1)) {
+                    continue;
+                }
+                let mut subterms: Vec<Vec<Expr>> = Vec::new();
+                for i in 0..*k {
+                    // println!("i is {}", i);
+                    // eprintln!("ns[i] is {}, subnt[i] is {:?}", ns[i as usize], subnt[i as usize]);
+                    // println!("ns, subnt are {:?}, {:?}", ns, subnt);
+                    // println!("b is {:?}", b);
+                    let subterm = b.get(&(ns[i as usize], subnt[i as usize])).unwrap();
+                    subterms.push(subterm.clone());
+                }
+                for subterm in subterms.iter().multi_cartesian_product() {
+                    // println!("subterm is {:?}", subterm);
+                    match transition {
+                        Transition::Append => match (subterm[0].clone(), subterm[1].clone()) {
+                            (Expr::S(s1), Expr::S(s2)) => {
+                                yield_!((
+                                    NonTerminal::S,
+                                    Expr::S(S::Append(Box::new(s1), Box::new(s2)))
+                                ));
+                            }
+                            _ => {
+                                panic! {"production list encoded with errorneous information"}
+                            }
+                        },
+                        Transition::SubString => {
+                            match (subterm[0].clone(), subterm[1].clone(), subterm[2].clone()) {
+                                (Expr::S(s1), Expr::N(n1), Expr::N(n2)) => {
+                                    yield_!((
+                                        NonTerminal::S,
+                                        Expr::S(S::SubString(
+                                            Box::new(s1),
+                                            Box::new(n1),
+                                            Box::new(n2)
+                                        ))
+                                    ));
+                                }
+                                _ => {
+                                    panic! {"production list encoded with errorneous information"}
+                                }
+                            }
+                        }
+                        Transition::Find => match (subterm[0].clone(), subterm[1].clone()) {
+                            (Expr::S(s1), Expr::S(s2)) => {
+                                yield_!((NonTerminal::N, Expr::N(N::Find(s1, s2))));
+                            }
+                            _ => {
+                                panic! {"production list encoded with errorneous information"}
+                            }
+                        },
+                        _ => {
+                            panic! {"production list encoded with errorneous information"}
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
 
-//     // base case, arity and height are 0
-//     // if n == 0 && k == 0 {
-//     //   return (NonTerminal::S, S::Input);
+/// https://stackoverflow.com/questions/44139493/in-rust-what-is-the-proper-way-to-replicate-pythons-repeat-parameter-in-iter
+/// Rust version of Python's itertools.product().
+/// It returns the cartesian product of the input iterables, and it is
+/// semantically equivalent to `repeat` nested for loops.
+///
+/// # Arguments
+///
+/// * `it` - An iterator over a cloneable data structure
+/// * `repeat` - Number of repetitions of the given iterator
+pub trait ProductRepeat: Iterator + Clone
+where
+    Self::Item: Clone,
+{
+    fn product_repeat(self, repeat: usize) -> MultiProduct<Self> {
+        std::iter::repeat(self)
+            .take(repeat)
+            .multi_cartesian_product()
+    }
+}
 
-
-//     // build subterms from the bank
-
-//     return (NonTerminal::S, S::Input);
-// }
+impl<T: Iterator + Clone> ProductRepeat for T where T::Item: Clone {}
--- src/string_dsl.rs
+++ src/string_dsl.rs
@@ -4,15 +4,31 @@
 // TODO: probably need to extend this to a trait to abstract away the grammar
 // where it shares the following operations:
 // eval
+use std::fmt;
 
-pub static PRODUCTION: &'static [(NonTerminal, u32, Transition, &[NonTerminal])] = &[
+pub static PRODUCTION: &[(NonTerminal, u32, Transition, &[NonTerminal])] = &[
     (NonTerminal::S, 0, Transition::Input, &[]),
     (NonTerminal::S, 0, Transition::Space, &[]),
-    (NonTerminal::S, 2, Transition::Append, &[NonTerminal::S, NonTerminal::S]),
-    (NonTerminal::S, 3, Transition::SubString, &[NonTerminal::S, NonTerminal::N, NonTerminal::N]),
+    (
+        NonTerminal::S,
+        2,
+        Transition::Append,
+        &[NonTerminal::S, NonTerminal::S],
+    ),
+    (
+        NonTerminal::S,
+        3,
+        Transition::SubString,
+        &[NonTerminal::S, NonTerminal::N, NonTerminal::N],
+    ),
     (NonTerminal::N, 0, Transition::Zero, &[]),
-    (NonTerminal::N, 2, Transition::Find, &[NonTerminal::S, NonTerminal::S])
-    ];
+    (
+        NonTerminal::N,
+        2,
+        Transition::Find,
+        &[NonTerminal::S, NonTerminal::S],
+    ),
+];
 
 // p.17 nadia STRINGY DSL
 #[derive(Clone, Debug)]
@@ -20,25 +36,45 @@ pub enum S {
     Input,
     Space,
     Append(Box<S>, Box<S>),
-    SubString(Box<S>, Box<N>, Box<N>)
+    SubString(Box<S>, Box<N>, Box<N>),
+}
+
+impl fmt::Display for S {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match self {
+            S::Input => write!(f, "x"),
+            S::Space => write!(f, "\"_\""),
+            S::Append(s1, s2) => write!(f, "{} ++ {}", s1, s2),
+            S::SubString(s, n1, n2) => write!(f, "{}[{}..{}])", s, n1, n2),
+        }
+    }
 }
 
 #[derive(Clone, Debug)]
 pub enum N {
     Zero,
-    Find(S, S)
+    Find(S, S),
+}
+
+impl fmt::Display for N {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match self {
+            N::Zero => write!(f, "0"),
+            N::Find(s1, s2) => write!(f, "find({}, {})", s1, s2),
+        }
+    }
 }
 
 #[derive(Clone, PartialEq, Eq, Hash, Debug, Copy)]
 pub enum NonTerminal {
     S,
-    N
+    N,
 }
 
 #[derive(Debug, Clone)]
 pub enum Expr {
     S(S),
-    N(N)
+    N(N),
 }
 
 #[derive(Clone, Debug)]
@@ -48,7 +84,7 @@ pub enum Transition {
     Append,
     SubString,
     Zero,
-    Find
+    Find,
 }
 
 // interpreter
@@ -61,6 +97,13 @@ pub fn eval(s: S, input: String) -> String {
             let s = eval(*s, input.clone());
             let n1 = eval_n(*n1, &input);
             let n2 = eval_n(*n2, &input);
+            // is n1 .. n2 does not make sense return empty string
+            if n1 > n2 {
+                return "".to_string();
+            }
+            if n2 > s.len() {
+                return "".to_string();
+            }
             s[n1..n2].to_string()
         }
     }
@@ -72,7 +115,7 @@ pub fn eval_n(n: N, input: &str) -> usize {
         N::Find(s1, s2) => {
             let s1 = eval(s1, input.to_owned());
             let s2 = eval(s2, input.to_owned());
-            s1.find(&s2).unwrap()
+            s1.find(&s2).unwrap_or(0)
         }
     }
 }
